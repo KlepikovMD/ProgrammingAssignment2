b05938d39a75367f26f35d12d0b6fcb05db8aef4
KlepikovMD <KlepikovMD@gmail.com>
2014-06-22 18:29:55 +0600

    Assignment completed!
    Implemented both functions!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverting a matrix is costly computation. Because of this, 
+## here is a "class" representing a pair of matrix and inverted
+## matrix, accessible vea getter and setter methods. makeCacheMatrix
+## makes a new instance of this class, and cacheSolve is a inversion 
+## function workong only on makeCacheMatrix instances. When 
+## called a fist time, it will cache the result in memory
+## and will return it again afterwards.
 
-## Write a short comment describing this function
+## Create instance of object representing both matrix and
+## it's inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mat <- x
+    inv_mat <- NULL
+    get <- function(){
+        mat
+    }
+    set <- function(new){
+        mat <<- new
+        inv_mat <<- NULL
+    }
+    getinv <- function(){
+        inv_mat
+    }
+    setinv <- function(new){
+        inv_mat <<- new
+    }
+    list(get = get, set = set, getinv = getinv, setinv = setinv)
 }
 
 
-## Write a short comment describing this function
+## Inverts the cacheMatrix. At first call will calculate inversion
+## and return it, on subsequent will return memorised result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        ## message("Returning cached result") ##UNCOMMENT FOR DEBUG
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinv(inv)
+    inv
 }
